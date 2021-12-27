80dfabfe907e5fcea85dc5b2d6c77701dec68c7f
vinit4a <vinit.k.addweb@gmail.com>
2021-12-27 11:02:21 +0530

    crud app using node

--- nodejs/crud app/app.js
+++ nodejs/crud app/app.js
@@ -1,74 +1,107 @@
-const path = require('path');
-const express = require('express');
-const ejs = require('ejs');
-const bodyParser = require('body-parser');
+const path = require("path");
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
 const app = express();
 
 // database connection
-const mysql = require('mysql');
-const connection=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'node_crud'
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "node_crud",
 });
- 
-connection.connect(function(error){
-    if(!!error) console.log(error);
-    else console.log('Database Connected!');
-}); 
 
+connection.connect(function (error) {
+  if (!!error) console.log(error);
+  else console.log("Database Connected!");
+});
 
 //set views file
-app.set('views',path.join(__dirname,'views'));
- 
+app.set("views", path.join(__dirname, "views"));
+
 //set view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
- 
 
+app.get("/", (req, res) => {
+  // res.send('index.html');
+  let sql = "SELECT * FROM users";
 
-app.get('/',(req,res)=>{
-    // res.send('index.html');
-    let sql = "SELECT * FROM users";
-    
-    let query = connection.query(sql,(err,rows)=>{
-        if(err) throw err;
-    
- 
-    res.render('user_index',{   
-        title:'Crud app using node js',
-        user:rows
-        
-    
+  let query = connection.query(sql, (err, rows) => {
+    if (err) throw err;
+
+    res.render("user_index", {
+      title: "Crud app using node js",
+      user: rows,
     });
-})
+  });
 });
 
-app.get('/add',(req,res)=>{
-    res.render('user_add',{   
-        title:'Crud app using node js',
-        
-        
-    
-    });;
+app.get("/add", (req, res) => {
+  res.render("user_add", {
+    title: "Crud app using node js",
+  });
+});
+
+app.post("/save", (req, res) => {
+  let data = {
+    name: req.body.name,
+    email: req.body.email,
+    phone_no: req.body.phone_no,
+  };
+  let sql = "INSERT  INTO users SET ?";
+  let query = connection.query(sql, data, (err, results) => {
+    if (err) throw err;
+    res.redirect("/");
+  });
+});
+
+// edit section
+app.get("/edit/:userId", (req, res) => {
+  const userId = req.params.userId;
+  let sql = `Select * from users where id = ${userId}`;
+  let query = connection.query(sql, (err, result) => {
+    if (err) throw err;
+    res.render("user_edit", {
+      title: "CRUD Operation using NodeJS / ExpressJS / MySQL",
+      user: result[0],
+    });
+  });
+});
+
+// update
+app.post("/update", (req, res) => {
+  const userId = req.body.id;
+  let sql =
+    "update users SET name='" +
+    req.body.name +
+    "',  email='" +
+    req.body.email +
+    "',  phone_no='" +
+    req.body.phone_no +
+    "' where id =" +
+    userId;
+  let query = connection.query(sql, (err, results) => {
+    if (err) throw err;
+    res.redirect("/");
+  });
 });
 
 
-app.post('/save',(req,res)=>{
-   let data = {name: req.body.name,email: req.body.email,phone_no: req.body.phone_no};
-    let sql =  "INSERT  INTO users SET ?";
-      let query = connection.query(sql,data,(err,results)=>{
-         if(err) throw err;
-         res.redirect('/');
-      });
-    });  
- 
+// delete 
+app.get('/delete/:userId',(req, res) => {
+    const userId = req.params.userId;
+    let sql = `DELETE from users where id = ${userId}`;
+    let query = connection.query(sql,(err, result) => {
+        if(err) throw err;
+        res.redirect('/');
+    });
+});
+
 // Server Listening
-app.listen(3600, () => {
-    console.log('Server is running at port 3800');
+app.listen(5000, () => {
+  console.log("Server is running at port 3800");
 });
- 
-
- 
